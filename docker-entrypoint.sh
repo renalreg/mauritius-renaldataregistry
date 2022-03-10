fc189a19b0f512a7bbddf9a859e4c12d95316703
Andre Ortega <andre.ortegaalban@renalregistry.nhs.uk>
2022-03-10 12:43:03 +0000

    Add loading of initial data for renaldataregistry to dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,10 @@
 echo "Apply database migrations"
 python src/manage.py migrate
 
+# Load initial data
+echo "Load initial data"
+python src/manage.py loaddata src/renaldataregistry/fixtures/data.json
+
 # Create a superuser if it does not exist
 echo "Creating default super user from environment variables"
 python src/manage.py createsuperuser --noinput
--- src/renaldataregistry/admin.py
+++ src/renaldataregistry/admin.py
@@ -3,8 +3,19 @@
 # Register your models here.
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
-from .models import PatientRegistration, Comorbidity
+from .models import (
+    PatientRegistration,
+    Comorbidity,
+    RenalDiagnosis,
+    Disability,
+    LaboratoryParameter,
+    Medication,
+)
 
 
 admin.site.register(Comorbidity)
+admin.site.register(RenalDiagnosis)
+admin.site.register(Disability)
+admin.site.register(LaboratoryParameter)
+admin.site.register(Medication)
 admin.site.register(PatientRegistration, SimpleHistoryAdmin)
