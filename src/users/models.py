d84bd511f686620c1e891382d0f6e78b74ee34ec
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-04-13 14:33:15 +0000

    Enable Python 3.8 compatibility

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -15,7 +17,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS: list[str] = []
+    REQUIRED_FIELDS: List[str] = []
 
     objects = CustomUserManager()  # type: ignore
 
